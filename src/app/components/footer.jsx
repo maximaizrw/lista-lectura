2dbc8aa3ecad1013007aacde1f905f61bb11c304
Maximiliano Damian Maiz <maximaizrw@gmail.com>
2023-07-23 23:06:40 -0300

    add footer

---
+++ src/app/components/footer.jsx
@@ -0,0 +1,12 @@
+'use client';
+
+const Footer = () => {
+    return (
+      <header className="w-full bg-white p-4 text-center text-zinc-500 border-t-2">
+        <h1 className="">By Maximiliano Maiz</h1>
+      </header>
+    );
+  };
+  
+
+export default Footer;
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import Header from './components/header'
+import Footer from './components/footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,7 +13,9 @@ export default function RootLayout({ children }) {
       <body className={inter.className}  >
 
         <Header />
-        {children}</body>
+        {children}
+        <Footer /></body>
+        
     </html>
   )
 }
